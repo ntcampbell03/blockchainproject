f875d6fc4357d72d8d455da27255b65d5cd38e12
ntcampbell03 <ntcampbell03@gmail.com>
2022-07-11 11:57:31 -0500

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -6,7 +6,8 @@ gpg = gnupg.GPG(gnupghome='/opt/homebrew/Cellar/gnupg')
 gpg.encoding = 'utf-8'
 
 class Wallet:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.sent = 0
         self.recieved = 0
         self.balance = self.getBalance()
@@ -29,10 +30,22 @@ class Wallet:
 
 class godWallet:
     def __init__(self, balance):
+        self.name = 'god'
         self.sent = 0
         self.recieved = 0
         self.balance = balance
         self.futureBalance = balance
+        self.key = self.generateKeys()
+
+    def generateKeys(self):
+        input_data = gpg.gen_key_input(
+            name_email = 'noahc@berkeley.edu',
+            no_protection = True,
+            key_type = 'RSA',
+            key_length = 1024)
+
+        self.key = gpg.gen_key(input_data)
+        return self.key
 
     def getBalance(self):
         pass
