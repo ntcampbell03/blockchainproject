8cdaa52b584217cf15905a8c0272d9d1fc5340ab
ntcampbell03 <ntcampbell03@gmail.com>
2022-07-11 11:58:41 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
 from blockchain import *
 from wallet import *
 
+import gnupg
+import os
+
+from blockchain import *
+
+gpg = gnupg.GPG(gnupghome='/opt/homebrew/Cellar/gnupg')
+gpg.encoding = 'utf-8'
+
 blockchain =  Blockchain()
 blockchain.difficulty = 5
 
 god = godWallet(100)
-me = Wallet()
-you = Wallet()
+me = Wallet('me')
+you = Wallet('you')
+
+t = Transaction(god, me, 10)
+u = Transaction(me, you, 3)
+v = Transaction(me, you, 2)
