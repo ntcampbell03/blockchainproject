7b8bcb5a9de37fd19efffd17950b1267b8a8308a
ntcampbell03 <41170757+ntcampbell03@users.noreply.github.com>
2022-07-12 16:37:14 -0500

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -2,11 +2,10 @@ from wallet import *
 import hashlib
 import time
 
-
 class Blockchain:
     def __init__(self):
-        self.chain = []
-        self.length = 0
+        self.chain = [Block(None, 0)]
+        self.length = 1
         self.difficulty = 1
         self.newTransactions = []
         self.miningReward = 10
@@ -25,23 +24,27 @@ class Blockchain:
             else:
                 print('Wallet has insufficient balance!')
         except NameError:
-            print('Wallet does not exist!')  # Try except doesnt work
+            print('Wallet does not exist!') #Try except doesnt work
+
 
     def addBlock(self):
-        for transaction in self.newTransactions:
-            if str(gpg.decrypt(str(transaction.signature), passphrase=transaction.reciever.name)) == transaction.transactionString:
-                transaction.sender.sent += transaction.amount
-                transaction.reciever.recieved += transaction.amount
-                transaction.sender.getBalance()
-                transaction.sender.futureBalance = transaction.sender.balance
-                transaction.reciever.getBalance()
-                transaction.reciever.futureBalance = transaction.reciever.balance
-        newBlock = Block(self.newTransactions, self.length - 1)
-        newBlock.mineBlock(self.difficulty)
-        self.length += 1
-        self.chain.append(newBlock)
-        self.newTransactions = []
-
+        if self.newTransactions:
+            for transaction in self.newTransactions:
+                if str(gpg.decrypt(str(transaction.signature), passphrase = transaction.reciever.name)) == transaction.transactionString:
+                    transaction.sender.sent += transaction.amount
+                    transaction.reciever.recieved += transaction.amount
+                    transaction.sender.getBalance()
+                    transaction.sender.futureBalance = transaction.sender.balance
+                    transaction.reciever.getBalance()
+                    transaction.reciever.futureBalance = transaction.reciever.balance
+            newBlock = Block(self.newTransactions, self.length)
+            newBlock.mineBlock(self.difficulty)
+            if self.getLastBlock().index <= newBlock.index:
+                self.length += 1
+                self.chain.append(newBlock)
+                self.newTransactions = []
+        else:
+            print("No pending transactions")
 
 class Block:
     def __init__(self, transactions, index):
@@ -54,16 +57,17 @@ class Block:
 
     def getTransactions(self):
         for transaction in self.transactions:
-            print(
-                f'{transaction.sender.name} to {transaction.reciever.name}: {transaction.amount}')
+            print(f'{transaction.sender.name} to {transaction.reciever.name}: {transaction.amount}')
 
     def calculateHash(self):
-        hashTransactions = ''
-        for transaction in self.transactions:
-            hashTransactions += transaction.transactionString
-        hashString = (
-            f'{hashTransactions}{self.prev}{self.index}{self.nonce}{self.time}').encode()
-        return hashlib.sha256(hashString).hexdigest()
+        if not self.transactions: #deal with genesis block
+            return 0
+        else:
+            hashTransactions = ''
+            for transaction in self.transactions:
+                hashTransactions += transaction.transactionString
+            hashString = (f'{hashTransactions}{self.prev}{self.index}{self.nonce}{self.time}').encode()
+            return hashlib.sha256(hashString).hexdigest()
 
     def mineBlock(self, difficulty):
         target = ''
@@ -72,7 +76,6 @@ class Block:
         while self.hash[0:difficulty] != target:
             self.nonce += 1
             self.hash = self.calculateHash()
-
 
 class Transaction:
     def __init__(self, sender, reciever, amount):
@@ -85,9 +88,9 @@ class Transaction:
 
     def getSignature(self):
         encrypted_data = gpg.encrypt(
-            self.transactionString,
-            self.reciever.key.fingerprint,
-            sign=self.sender.key.fingerprint,
-            passphrase=self.reciever.name)
+        self.transactionString,
+        self.reciever.key.fingerprint,
+        sign=self.sender.key.fingerprint,
+        passphrase = self.reciever.name)
         self.signature = encrypted_data
         return encrypted_data
