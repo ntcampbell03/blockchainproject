137435aa0e17bd8c6a6abb5c254275c4f88a3be2
Noah Campbell <ntcampbell03@gmail.com>
2022-07-13 11:34:21 -0500

    gitignore testing

--- wallet.py
+++ wallet.py
@@ -2,33 +2,40 @@ import gnupg
 import os
 from blockchain import *
 
-gpg = gnupg.GPG(gnupghome='/opt/homebrew/Cellar/gnupg')
-# gpg = gnupg.GPG(gnupghome='/Users/noahcampbell/Desktop/CS/blockchainproject/env/lib/python3.10/site-packages')
-
+gpg = gnupg.GPG(gnupghome='/Users/noahcampbell/Desktop/CS/blockchainproject/env/lib/python3.10/site-packages')
 gpg.encoding = 'utf-8'
+
 
 class Wallet:
     def __init__(self, name):
-        self.name = name
+        self.name = str(name)
         self.sent = 0
         self.recieved = 0
         self.balance = self.getBalance()
         self.futureBalance = self.balance
-        self.key = self.generateKeys()
-
-    def generateKeys(self):
-        input_data = gpg.gen_key_input(
-            name_email='noahc@berkeley.edu',
+        self.input_data = gpg.gen_key_input(
+            name_real='hello',
             no_protection=True,
             key_type='RSA',
             key_length=1024)
+        self.key = gpg.gen_key(self.input_data)
 
+    def generateKeys(self):
+        input_data = gpg.gen_key_input(
+            name_real='hello',
+            no_protection=True,
+            key_type='RSA',
+            key_length=1024)
         self.key = gpg.gen_key(input_data)
         return self.key
 
     def getBalance(self):
         self.balance = self.recieved - self.sent
         return self.balance
+
+    
+
+
 
 
 class godWallet:
@@ -41,14 +48,7 @@ class godWallet:
         self.key = self.generateKeys()
 
     def generateKeys(self):
-        input_data = gpg.gen_key_input(
-            name_email='noahc@berkeley.edu',
-            no_protection=True,
-            key_type='RSA',
-            key_length=1024)
-
-        self.key = gpg.gen_key(input_data)
-        return self.key
+        return gpg.gen_key(gpg.gen_key_input(name_real = 'noah', no_protection = True, key_type = 'RSA', key_length = 1024))
 
     def getBalance(self):
         pass
