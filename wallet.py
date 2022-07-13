64a396bc89ade52d9201ab25a17729c97b2b50cc
Rithwik <rithwikbabu2020@gmail.com>
2022-07-13 12:09:24 -0500

    nothing

--- wallet.py
+++ wallet.py
@@ -2,7 +2,7 @@ import gnupg
 import os
 from blockchain import *
 
-gpg = gnupg.GPG(gnupghome='/Users/noahcampbell/Desktop/CS/blockchainproject/env/lib/python3.10/site-packages')
+gpg = gnupg.GPG(gnupghome='/opt/homebrew/Cellar/gnupg')
 gpg.encoding = 'utf-8'
 
 
