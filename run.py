947aa71b6fe8fbe19007cc90e19a5e43ee78c098
Noah Campbell <ntcampbell03@gmail.com>
2022-07-18 11:32:03 -0500

    gossip

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from coin import app
 from gossip.gossip_main import main as run_gossip
-    
+from examples.connections_simulation import Simulation
+
 if __name__ == "__main__":
     run_gossip()
     app.run(host='0.0.0.0', port=80)
-    
+    sim = Simulation(number_clients=30, number_connections_per_client=5)
+    sim.run()
