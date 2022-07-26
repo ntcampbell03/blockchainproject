96358faa1d708ba49ec71cdbce1caa57986334eb
Rithwik <rithwikbabu2020@gmail.com>
2022-07-26 11:26:19 -0500

    what

--- blockchain.py
+++ blockchain.py
@@ -4,7 +4,8 @@ import math
 import gnupg
 import json
 
-gpg = gnupg.GPG(gnupghome='/usr/bin')
+# gpg = gnupg.GPG(gnupghome='/usr/bin')
+gpg = gnupg.GPG()
 # gpg = gnupg.GPG(gnupghome='/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages')
 # gpg = gnupg.GPG(gnupghome='/Users/nikhiljain/Desktop/blockchainproject/env/lib/python3.9/site-packages/')
 # gpg = gnupg.GPG(gnupghome='/Users/rithwikbabu/Documents/appcode/blockchainproject/env/lib/python3.9/site-packages/')
