55ce10031a063f1d5a8daab35cbc1c68d40a5ce6
Noah Campbell <41170757+ntcampbell03@users.noreply.github.com>
2022-07-26 00:27:30 -0500

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -3,8 +3,8 @@ import time
 import math
 import gnupg
 
-# gpg = gnupg.GPG(gnupghome='/usr/bin')
-gpg = gnupg.GPG(gnupghome='/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages')
+gpg = gnupg.GPG(gnupghome='/usr/bin')
+# gpg = gnupg.GPG(gnupghome='/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages')
 # gpg = gnupg.GPG(gnupghome='/Users/nikhiljain/Desktop/blockchainproject/env/lib/python3.9/site-packages/')
 # gpg = gnupg.GPG(gnupghome='/Users/rithwikbabu/Documents/appcode/blockchainproject/env/lib/python3.9/site-packages/')
 
